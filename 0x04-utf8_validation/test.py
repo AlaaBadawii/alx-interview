b5cdd5b3905a0310e74906a98827c9c3f2ee6294
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-24 14:51:57 +0300

    check

--- 0x04-utf8_validation/test.py
+++ 0x04-utf8_validation/test.py
@@ -1,11 +1,5 @@
-#!/usr/bin/python3
-"""UTF-8 validation module.
-"""
-
-
 def validUTF8(data):
-    """Checks if a list of integers are valid UTF-8 codepoints.
-    """
+
     skip = 0
     n = len(data)
     for i in range(n):
@@ -14,29 +8,34 @@ def validUTF8(data):
             continue
         if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
             return False
-        span = 0  # Initialize span to a default value
-        if data[i] <= 127:
+        span = 0
+        if data[i] < 128:
             skip = 0
-        elif data[i] & 0b11111000 == 0b11110000:
-            # 4-byte utf-8 character encoding
+        elif data[i] & 0b1111000 == 0b11110000:
             span = 4
-        elif data[i] & 0b11110000 == 0b11100000:
-            # 3-byte utf-8 character encoding
+        elif data[i] & 0b1110000 == 0b11100000:
             span = 3
-        elif data[i] & 0b11100000 == 0b11000000:
-            # 2-byte utf-8 character encoding
+        elif data[i] & 0b1100000 == 0b11000000:
             span = 2
         else:
             return False
-
         if n - i >= span:
-            next_body = list(map(
+            continuation = list(map(
                 lambda x: x & 0b11000000 == 0b10000000,
                 data[i + 1: i + span],
             ))
-            if not all(next_body):
+            if not all(continuation):
                 return False
             skip = span - 1
         else:
             return False
     return True
+
+data = [65]
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
