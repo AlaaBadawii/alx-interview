ead75bdd3517b36665439ae3053037a49e1ee63a
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-09 01:16:22 +0300

    check 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,28 +3,23 @@ const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-	const filmId = process.argv[2];
-	request(`${API_URL}/films/${filmId}/`, (err, _, body) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-		const charactersURL = JSON.parse(body).characters;
-		const charactersName = charactersURL.map(url =>
-			new Promise((resolve, reject) => {
-				request(url, (promiseErr, __, charactersReqBody) => {
-					if (promiseErr) {
-						reject(promiseErr);
-						return;
-					}
-					resolve(JSON.parse(charactersReqBody).name);
-				});
-			})
-		);
-
-		Promise.all(charactersName)
-			.then(names => console.log(names.join('\n')))
-			.catch(allErr => console.error(allErr));
-	});
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
 }
